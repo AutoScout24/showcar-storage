8e98fd98afc715171dba35b9a5fbbcf14958f609
Simon Waibl <swaibl.ext@autoscout24.com>
2015-10-20 10:41:02 +0200

    add testrunner configuration

---
+++ karma.conf.js
@@ -0,0 +1,23 @@
+module.exports = function(config) {
+    config.set({
+        basePath: '',
+        frameworks: ['jasmine'],
+        files: [
+          'index.js',
+          'test/*.spec.js'
+        ],
+        exclude: [],
+        preprocessors: {
+            'index.js': ['webpack'],
+            'test/*.spec.js': ['webpack']
+        },
+        webpack: {},
+        reporters: ['progress'],
+        port: 9876,
+        colors: true,
+        logLevel: config.LOG_INFO,
+        autoWatch: false,
+        browsers: ['Chrome'],
+        singleRun: true
+    })
+};
