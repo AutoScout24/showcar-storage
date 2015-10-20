56e3998010bb69389afd6e302dd1b07f48a2d003
Simon Waibl <swaibl.ext@autoscout24.com>
2015-10-20 15:08:47 +0200

    implement setter of the localStore, refactor tests

--- test/stores/local.spec.js
+++ test/stores/local.spec.js
@@ -3,15 +3,27 @@ var LocalStore = require("../../dist/stores/local.js");
 
 describe("The localStore", function () {
     describe("The accessors", function () {
-        it("should implement a getter", function () {
-            localStorage.clear();
-            localStorage.setItem('myKey', 'myValue');
+        var localStore;
 
-            var localStore = new LocalStore();
+        beforeEach(function () {
+            localStorage.clear();
+
+            localStore = new LocalStore();
+        });
+
+        it("should implement a getter", function () {
+            localStorage.setItem('myKey', 'myValue');
 
             var value = localStore.get('myKey');
 
             expect(value).toBe('myValue');
         });
+
+        it("should implement a setter", function () {
+            localStore.set('myKey', 'myValue');
+
+            expect(localStorage.getItem('myKey')).toBe('myValue');
+        });
+
     });
 });
