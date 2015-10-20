ac46119c367bc1dc7f90925902af06fd03d44f4f
Simon Waibl <swaibl.ext@autoscout24.com>
2015-10-20 10:42:48 +0200

    add base structure and test

---
+++ test/index.spec.js
@@ -0,0 +1,8 @@
+var Storage = require("../index.js");
+
+describe("The Showcar Storage Module", function () {
+    it("should be of the supplied type", function () {
+        var storage = new Storage("local");
+        expect(storage.type).toBe("local");
+    });
+});
