00f617b78f490adb4001ad8d4e649e31d945faa0
Simon Waibl <swaibl.ext@autoscout24.com>
2015-10-29 09:00:41 +0100

    refactor cookieStore test

--- test/stores/cookie.spec.js
+++ test/stores/cookie.spec.js
@@ -2,38 +2,36 @@
 var CookieStore = require("../../dist/stores/cookie.js");
 
 describe("The cookieStore", function () {
+    let cookieStore;
+
+    const key = "myعKöy", value = "myVثاälue";
+    const pair = [key, value]
+        .map(encodeURIComponent)
+        .join("=");
+
     describe("The accessors", function () {
-        function clearAllCookies() {
-            var replaceDate = "=;expires=" + new Date().toUTCString() + ";path=/";
-            document.cookie
-                .split(";")
-                .forEach(function(c) {
-                    document.cookie = c.replace(/^ +/, "").replace(/=.*/, replaceDate);
-                });
-        }
-
-        var cookieStore;
-
         beforeEach(function () {
             clearAllCookies();
             cookieStore = new CookieStore();
         });
 
-        it("should implement a getter", function () {
-            document.cookie = 'a=b;path=/foo';
-            document.cookie = `${encodeURIComponent('myعKöy')}=${encodeURIComponent('myVثاälue')}`;
-            document.cookie = 'c=d;secure';
+        it("should implement a getter returning the correct value for a key found", function () {
+            ['a=b;path=/foo', pair, 'c=d;secure']
+                .forEach(e => document.cookie = e);
 
-            var value = cookieStore.get('myعKöy');
-
-            expect(value).toBe('myVثاälue');
+            expect(cookieStore.get(key)).toBe(value);
         });
+
         it("should implement a setter", function () {
-            cookieStore.set('myعKöy', 'myVثاälue');
+            cookieStore.set(key, value);
 
-            var keyValPair = `${encodeURIComponent('myعKöy')}=${encodeURIComponent('myVثاälue')}`;
-
-            expect(document.cookie.indexOf(keyValPair)).toBeGreaterThan(-1);
+            expect(document.cookie.indexOf(pair)).toBeGreaterThan(-1);
         });
     });
+
+    const clearAllCookies = () => {
+        let replacement = `=;expires=${new Date().toUTCString()};path=/`;
+        let replacer = c => c.trim().replace(/=.*/, replacement);
+        document.cookie.split(";").map(replacer).forEach(c => document.cookie = c);
+    };
 });
