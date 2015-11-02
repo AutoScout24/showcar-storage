4cc4e611913767815799473fc729552aa69872d0
Simon Waibl <swaibl.ext@autoscout24.com>
2015-11-02 10:49:58 +0100

    update build and test chain

--- karma.conf.js
+++ karma.conf.js
@@ -1,11 +1,9 @@
-module.exports = function(config) {
+module.exports = function (config) {
     config.set({
         basePath: '',
         frameworks: ['jasmine'],
         files: ['test/**/*.spec.js'],
-        exclude: [],
         preprocessors: {
-            'index.js': ['webpack'],
             'test/**/*.spec.js': ['webpack']
         },
         webpack: {},
--- webpack.conf.js
+++ webpack.conf.js
@@ -1,20 +1,10 @@
 module.exports = {
-    entry: "./lib/storage.js",
+    entry: "./dist/storage.js",
     output: {
         path: "./dist",
         filename: "storage.min.js"
     },
     plugins: [
         new (require("webpack")).optimize.UglifyJsPlugin({minimize: true})
-    ],
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                loader: "babel",
-                query: {presets: ["es2015"]},
-                exclude: /(node_modules|dist|bower_components)/
-            }
-        ]
-    }
+    ]
 };
